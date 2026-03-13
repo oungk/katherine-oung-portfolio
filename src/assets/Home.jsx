5093041161eafc3062905ed1cb199d4936619e04
katherineoung <katherine.oung@gmail.com>
2026-03-13 10:51:35 -0500

    summary syntax changes

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -85,7 +85,7 @@ export default function Home() {
         maxWidth: '400px',
         margin: 0
       }}>
-        is a data reporter at the <em>Houston Chronicle</em>. They previously interned on the Interactive News team at <em>The New York Times</em>. Their debut poetry chapbook, <em>Desire Path</em>, is forthcoming with <em>Chestnut Review</em>. They hope you have a wonderful day.
+        is a data reporter at the Houston Chronicle. They previously interned on the Interactive News team at The New York Times. Their debut poetry chapbook, <em>Desire Path</em>, is forthcoming with Chestnut Review. They hope you have a wonderful day.
       </p>
 
       {/* Decorative bottom text */}
